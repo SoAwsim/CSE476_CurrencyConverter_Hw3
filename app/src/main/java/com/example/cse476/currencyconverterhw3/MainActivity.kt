f06e9ef2f9278e76862d2f053be1de3829e7966d
Oğuzhan İçelliler <oguzhan.icelliler@gmail.com>
2024-11-23 01:45:52 +0300

    feat: initial support for text box state handling

--- app/src/main/java/com/example/cse476/currencyconverterhw3/MainActivity.kt
+++ app/src/main/java/com/example/cse476/currencyconverterhw3/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.example.cse476.currencyconverterhw3
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.launch
+import com.example.cse476.currencyconverterhw3.extensions.toUIString
+import com.example.cse476.currencyconverterhw3.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    private val model: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,8 +26,47 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        this.findViewById<EditText>(R.id.currencyConvertFromValue).apply {
 
+        val editTextFrom = this.findViewById<EditText>(R.id.currencyConvertFromValue)
+        val editTextTo = this.findViewById<EditText>(R.id.currencyConvertToValue)
+
+        model.currencyFieldState.observe(this) { state ->
+            val fromValue = state.currencyFromNumber
+
+            // Prevent unnecessary event firing
+            if (editTextFrom.text.toString().toDoubleOrNull() != fromValue)
+                editTextFrom.setText(fromValue.toUIString())
+
+            editTextTo.setText(state.currencyToNumber.toUIString())
+        }
+
+        editTextFrom.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { }
+
+            override fun afterTextChanged(s: Editable?) {
+                val input = (s ?: "").toString()
+                if (input.isEmpty()) {
+                    model.updateFromCurrency(null)
+                    return
+                }
+
+                val numericValue = input.toDoubleOrNull()
+                model.updateFromCurrency(numericValue)
+                if (numericValue == null) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Please enter a valid number",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        })
+
+        val convertButton = this.findViewById<Button>(R.id.button)
+
+        convertButton.setOnClickListener {
+            model.convertButton()
         }
     }
 }
--- app/src/main/java/com/example/cse476/currencyconverterhw3/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/cse476/currencyconverterhw3/viewmodel/MainViewModel.kt
@@ -1,16 +1,30 @@
 package com.example.cse476.currencyconverterhw3.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 
-data class MainUiState(
+data class CurrencyFieldState(
     val currencyFromNumber: Double? = null,
     val currencyToNumber: Double? = null
 )
 
 class MainViewModel: ViewModel() {
-    private val _uiState = MutableStateFlow(MainUiState())
-    val uiState: StateFlow<MainUiState> = _uiState.asStateFlow()
+    private val _currencyFieldState = MutableLiveData(CurrencyFieldState())
+    val currencyFieldState: LiveData<CurrencyFieldState> = _currencyFieldState
+
+    fun updateFromCurrency(value: Double?) {
+        _currencyFieldState.value = CurrencyFieldState(
+            currencyFromNumber = value,
+            currencyToNumber = null
+        )
+    }
+
+    fun convertButton() {
+        val fromNumber = this._currencyFieldState.value?.currencyFromNumber
+        _currencyFieldState.value = CurrencyFieldState(
+            currencyFromNumber = fromNumber,
+            currencyToNumber = 20.0
+        )
+    }
 }
