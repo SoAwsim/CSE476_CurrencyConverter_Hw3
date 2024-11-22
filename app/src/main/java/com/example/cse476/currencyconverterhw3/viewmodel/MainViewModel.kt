a383f3dc817c28799d7402f0b977cd8074f0816d
Oğuzhan İçelliler <oguzhan.icelliler@gmail.com>
2024-11-22 23:42:07 +0300

    feat: initial commit

---
+++ app/src/main/java/com/example/cse476/currencyconverterhw3/viewmodel/MainViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.cse476.currencyconverterhw3.viewmodel
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+data class MainUiState(
+    val currencyFromNumber: Double? = null,
+    val currencyToNumber: Double? = null
+)
+
+class MainViewModel: ViewModel() {
+    private val _uiState = MutableStateFlow(MainUiState())
+    val uiState: StateFlow<MainUiState> = _uiState.asStateFlow()
+}
