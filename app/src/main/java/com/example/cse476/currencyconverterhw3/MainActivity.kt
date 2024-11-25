ba7db53f1a1ade4bd6192f0d7570bf0e5a3cbb4f
Oğuzhan İçelliler <oguzhan.icelliler@gmail.com>
2024-11-26 00:55:19 +0300

    refactor: remove unused import

--- app/src/main/java/com/example/cse476/currencyconverterhw3/MainActivity.kt
+++ app/src/main/java/com/example/cse476/currencyconverterhw3/MainActivity.kt
@@ -6,7 +6,6 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
